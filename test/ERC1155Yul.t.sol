8148407e91fc92c5639d4607c4ce58baf63da353
Ratimon <ratixoxo@gmail.com>
2023-06-08 09:15:13 +0700

    Test (./test): Adding testSafeBatchTransferFromToEOA

--- test/ERC1155Yul.t.sol
+++ test/ERC1155Yul.t.sol
@@ -114,6 +114,53 @@ contract ERC1155YulTest is Test {
         assertEq(token.balanceOf(address(this), 1337), 30);
     }
 
+    function testSafeBatchTransferFromToEOA() public {
+        address from = address(0xABCD);
+
+        uint256[] memory ids = new uint256[](5);
+        ids[0] = 1337;
+        ids[1] = 1338;
+        ids[2] = 1339;
+        ids[3] = 1340;
+        ids[4] = 1341;
+
+        uint256[] memory mintAmounts = new uint256[](5);
+        mintAmounts[0] = 100;
+        mintAmounts[1] = 200;
+        mintAmounts[2] = 300;
+        mintAmounts[3] = 400;
+        mintAmounts[4] = 500;
+
+        uint256[] memory transferAmounts = new uint256[](5);
+        transferAmounts[0] = 50;
+        transferAmounts[1] = 100;
+        transferAmounts[2] = 150;
+        transferAmounts[3] = 200;
+        transferAmounts[4] = 250;
+
+        token.batchMint(from, ids, mintAmounts, "");
+
+        vm.prank(from);
+        token.setApprovalForAll(address(this), true);
+
+        token.safeBatchTransferFrom(from, address(0xBEEF), ids, transferAmounts, "");
+
+        assertEq(token.balanceOf(from, 1337), 50);
+        assertEq(token.balanceOf(address(0xBEEF), 1337), 50);
+
+        assertEq(token.balanceOf(from, 1338), 100);
+        assertEq(token.balanceOf(address(0xBEEF), 1338), 100);
+
+        assertEq(token.balanceOf(from, 1339), 150);
+        assertEq(token.balanceOf(address(0xBEEF), 1339), 150);
+
+        assertEq(token.balanceOf(from, 1340), 200);
+        assertEq(token.balanceOf(address(0xBEEF), 1340), 200);
+
+        assertEq(token.balanceOf(from, 1341), 250);
+        assertEq(token.balanceOf(address(0xBEEF), 1341), 250);
+    }
+
     function testBatchBalanceOf() public {
         address[] memory tos = new address[](5);
         tos[0] = address(0xBEEF);
